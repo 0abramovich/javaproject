84fab4ec98485c773836c40db37df166a22b25cb
0abramovich <abramovich.volha@gmail.com>
2024-06-17 00:17:42 +0300

    Update FactorialTest.java

--- src/test/java/lab/lesson7/FactorialTest.java
+++ src/test/java/lab/lesson7/FactorialTest.java
@@ -29,32 +29,6 @@ public class FactorialTest {
             assertEquals(calculator.calculateFactorial(1), 1); // Граничное значение 1
         }
     }
-=======
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class FactorialTest {
-    @Test
-    public void testCalculateFactorial() {
-        Factorial calculator = new Factorial();
-        assertEquals(1, calculator.calculateFactorial(0));
-        assertEquals(1, calculator.calculateFactorial(1));
-        assertEquals(2, calculator.calculateFactorial(2));
-        assertEquals(6, calculator.calculateFactorial(3));
-        assertEquals(24, calculator.calculateFactorial(4));
-        assertEquals(120, calculator.calculateFactorial(5));
-        assertEquals(40320, calculator.calculateFactorial(8));
-
-    }
-
-    @Test
-    public void testCalculateFactorialBoundary() {
-        Factorial calculator = new Factorial();
-        assertEquals(1, calculator.calculateFactorial(0)); // Граничное значение 0
-        assertEquals(1, calculator.calculateFactorial(1)); // Граничное значение 1
-    }
-
-
-}
 
 
